8832a6bda81aa034d2697739e75b6e7897168625
DivakarArjunsureka <Divakar2397@gmail.com>
2022-10-06 20:07:10 +0530

    sureka

--- src/test/java/GIT1.java
+++ src/test/java/GIT1.java
@@ -4,6 +4,11 @@ void empName() {
 		
 		System.out.println("Employee Name: Divakar");
 	}
+
+private void empMobile() {
+	System.out.println("emp mobie :123456789");
+
+}
 	public static void main(String[] args) {
 		
 		
